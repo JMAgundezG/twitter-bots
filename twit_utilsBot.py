8708c21dd238c584c08fb8115d1a5a8b38b20798
jmagundezg <jmagundezg@gmail.com>
2017-04-24 01:02:55 +0200

    first commit

---
+++ twit_utilsBot.py
@@ -0,0 +1,30 @@
+import time
+
+class twit_utilsBot(object):
+    def __init__(self):
+        self.bot = Account()
+
+    @staticmethod
+    def count_word(list_of_words):
+        aux = {}
+        for i in list_of_words:
+            if i in aux:
+                aux[i]+=1
+            else:
+                aux[i] = 1
+
+        a = sorted(aux.items(),key=lambda x:x[1],reverse=True)
+        return a[0]
+
+
+    def take_tweets_words(self,user):
+        tweets = self.bot.user_timeline(user, count=1000)
+        tweets_words = []
+        for i in tweets:
+            for j in i.text.split(" "):
+                tweets_words.append(j)
+        return tweets_words
+
+
+if __name__ == '__main__':
+    print(twit_utilsBot().count_word(["a","aaa","b","b"]))
