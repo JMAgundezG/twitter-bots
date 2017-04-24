aaf63ee3398bc43ac590d25545131132215d7755
trjano <clichade@alumnos.unex.es>
2017-04-24 16:02:50 +0200

    añadida la función mi crush

--- twit_utilsBot.py
+++ twit_utilsBot.py
@@ -18,31 +18,58 @@ class twit_utilsBot(object):
 
         a = sorted(aux.items(),key=lambda x:x[1],reverse=True)
         for w in a :
-            if len(w[0]) >4 and w[0] != 'RT':
+            #si tiene un tamaño mayor a 3, no es RT y no es una mención
+            if len(w[0]) >3 and w[0] != 'RT' and '@' not in w[0]:
                 return w
+        return a[0]
+
+    """devuelve la persona mas veces mencionada como un diccionario"""
+    @staticmethod
+    def count_mention(list_of_words):
+        aux = {}
+        for i in list_of_words:
+            if i in aux:
+                aux[i]+=1
+            elif '@' in i:
+                aux[i] = 1
+
+        a = sorted(aux.items(),key=lambda x:x[1],reverse=True)
         return a[0]
 
 
     def take_tweets_words(self,screen_name):
-
-
         tweets_words = []
-        for i in tweepy.Cursor(self.bot.user_timeline,screen_name=screen_name).items(400):#devuelve los 200 ultimos tweets
+        #obtenemos los ultimos 400 tweets de una persona
+        for i in tweepy.Cursor(self.bot.user_timeline,screen_name=screen_name).items(400):
             for j in i.text.split(" "):
                 tweets_words.append(j)
         return tweets_words
 
 
     def listener(self, tweet):
+
         if "#cuentame" in tweet['text']:#si se está utilizando el hastag cuentame
 
             #obtenemos una tupla con la palbra mas usada y el número de veces de esta
-            words = self.count_word(self.take_tweets_words(tweet['user']['screen_name']))
-            #creamos el string del tweet aunque sea un poco lioso
+            word_list = self.take_tweets_words(tweet['user']['screen_name'])
+            word = self.count_word(word_list)
 
-            s = str('@'+tweet['user']['screen_name']+" tu palabra mas usada: "+ words[0]+"\nexactamente "+ str(words[1])+" veces")
+            #creamos el string del tweet aunque sea un poco lioso
+            s = str('@'+tweet['user']['screen_name']+" tu palabra mas usada: "+ word[0]+"\nexactamente "+ str(word[1])+" veces")
             #twiteamos
             self.bot.update_status(s)
-            print("Tu palabra mas usada ", words)
+            print("Tu palabra mas usada ", word)
+
+        elif "#miCrush" in tweet['text']:
+            word_list = self.take_tweets_words(tweet['user']['screen_name'])
+            word = self.count_mention(word_list)
+            # creamos el string del tweet aunque sea un poco lioso
+            s = str('@' + tweet['user']['screen_name'] + " mayor mención: " + word[0] + "\ntotal " + str(
+                word[1]) + " veces")
+            self.bot.update_status(s)
+            print('@' + tweet['user']['screen_name'] + " mayor mención: " + word[0] + "\ntotal " + str(
+                word[1]) + " veces")
+
+
 
 
